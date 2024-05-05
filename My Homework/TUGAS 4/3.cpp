fdefa98f6c4c6a0fe2c435792cfb5b33f9c0f468
achwahyu036di <164317117+achwahyu036di@users.noreply.github.com>
2024-05-05 19:22:34 +0700

    Add files via upload

---
+++ My Homework/TUGAS 4/3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+    int n = 1, r, s, total = 0;
+    for (int r = n; r <= 10; ++r){
+    for (int s = n; s <=10; ++s) {
+        cout 
+        } 
+    }
+    }

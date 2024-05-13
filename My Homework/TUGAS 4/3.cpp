8d05ea8982d8dd0b86fcecd994d077b535568098
achwahyu036di <164317117+achwahyu036di@users.noreply.github.com>
2024-05-13 18:13:29 +0700

    Update 3.cpp

--- My Homework/TUGAS 4/3.cpp
+++ My Homework/TUGAS 4/3.cpp
@@ -1,30 +1,22 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main() {
-  // Deklarasi variabel
-  int baris, total = 0, genap;
+    int y,z;
+    int sum;
+    int input;
+    for(y = 2; y <= 10; y += 2) {
+        sum = 0;
+	      for(z = 2; z <= y; z += 2) {
+            if(z != 2)
+            cout << " + "; 
+            cout << z;
+   	        sum = sum + z;
+            } 
+      cout << " = " << sum << endl;
+   }
 
-  // Meminta input jumlah baris
-  cout << "Masukkan jumlah baris: ";
-  cin >> baris;
-
-  // Perulangan untuk setiap baris
-  for (int i = 1; i <= baris; i++) {
-    // Menginisialisasi variabel genap
-    genap = 2;
-
-    // Menghitung dan menampilkan penjumlahan deret bilangan genap
-    for (int j = 1; j <= i; j++) {
-      cout << genap << " ";
-      total += genap;
-      genap += 2;
-    }
-
-    cout << "= " << total << endl;
-    total = 0; // Reset total untuk baris berikutnya
-  }
-
-  return 0;
+return 0;
 }

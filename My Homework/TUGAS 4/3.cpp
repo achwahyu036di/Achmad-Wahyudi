95263096a18522131d24cfb3ff9bd85af1c4efdc
achwahyu036di <164317117+achwahyu036di@users.noreply.github.com>
2024-05-13 16:58:33 +0700

    Update 3.cpp

--- My Homework/TUGAS 4/3.cpp
+++ My Homework/TUGAS 4/3.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
-#include <stdio.h>
-#include <conio.h>
 
 using namespace std;
 
 int main() {
-    int n = 1, r, s, total = 0;
-    for (int r = n; r <= 10; ++r){
-    for (int s = n; s <=10; ++s) {
-        cout 
-        } 
+  // Deklarasi variabel
+  int baris, total = 0, genap;
+
+  // Meminta input jumlah baris
+  cout << "Masukkan jumlah baris: ";
+  cin >> baris;
+
+  // Perulangan untuk setiap baris
+  for (int i = 1; i <= baris; i++) {
+    // Menginisialisasi variabel genap
+    genap = 2;
+
+    // Menghitung dan menampilkan penjumlahan deret bilangan genap
+    for (int j = 1; j <= i; j++) {
+      cout << genap << " ";
+      total += genap;
+      genap += 2;
     }
-    }
+
+    cout << "= " << total << endl;
+    total = 0; // Reset total untuk baris berikutnya
+  }
+
+  return 0;
+}
